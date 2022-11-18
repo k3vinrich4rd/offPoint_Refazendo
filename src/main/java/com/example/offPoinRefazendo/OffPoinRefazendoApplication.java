51ace34c8f7a56390eacd783483352b976d67b54
k3vinrich4rd <kevinrichardve@gmail.com>
2022-11-18 15:19:25 -0300

    Inserindo escopo do projeto

---
+++ src/main/java/com/example/offPoinRefazendo/OffPoinRefazendoApplication.java
@@ -0,0 +1,13 @@
+package com.example.offPoinRefazendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffPoinRefazendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffPoinRefazendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/offPoinRefazendo/OffPoinRefazendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offPoinRefazendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffPoinRefazendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
